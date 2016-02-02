02e6ea8274663ef3da15cd2a3bb3643f4799cc3c
ProductlabAle <iot@productlab.it>
2016-02-02 17:29:57 +0100

    add json

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,5 @@
 app.controller("MainController", ["$scope", function ($scope) {
 	$scope.title = "Benvenuto in LiveMap";
 	$scope.logo = "http://192.168.1.1/altervista_root/cms/livemap/img/home_cms.png";
+	$scope.pitName = "D:\angular\project1\json\pits.json";
 }]);
