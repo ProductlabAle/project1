cbdc30744ee681966cd236e88cbfa89668b48ee9
ProductlabAle <iot@productlab.it>
2016-02-03 12:52:08 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-var app = angular.module("Livemap", []); //viene crato un modulo con il nome myApp e un array vuoto
+(function () {
+	var app = angular.module("livemap", []); //viene crato un modulo con il nome Livemap e un array vuoto (dependencies)
+	app.controller("PitList", function () {
+		this.pits = ids;
+	});
+	var ids = (D:\angular\project1\json\pits.json);
+})();
